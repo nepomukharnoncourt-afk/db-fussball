ebf7e69d2b597519b6db4dfcb2e0386d06b34c75
nepomukharnoncourt-afk <nepomuk.harnoncourt@mng.ch>
2026-01-15 13:59:25 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -10,40 +10,40 @@ CREATE TABLE todos (
     content VARCHAR(100),
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
-    );
+);
+
+CREATE TABLE Liga (
+    liganr INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20),
+    land VARCHAR(20)
+);
+
+CREATE TABLE Clubs (
+    teamnr INT AUTO_INCREMENT PRIMARY KEY,
+    liga INT NOT NULL,
+    tore INT,
+    gegentore INT,
+    name VARCHAR(30),
+    platzierung INT,
+    FOREIGN KEY (liga) REFERENCES Liga(liganr)
+);
 
 CREATE TABLE Spieler (
     spielernr INT AUTO_INCREMENT PRIMARY KEY,
+    team INT NOT NULL,
     vorname VARCHAR(20),
     nachname VARCHAR(20),
     tore INT,
     vorlagen INT,
     marktwert INT,
     position VARCHAR(20),
-    FOREIGN KEY (team) REFERENCES Club(teamnr)
-    );
-
-
-CREATE TABLE Clubs (
-    teamnr INT AUTO_INCREMENT PRIMARY KEY,
-    tore INT,
-    gegentore INT,
-    name VARCHAR(30),
-    platzierung INT,
-    FOREIGN KEY (liga) REFERENCES Liga(liganr)
-    );
+    FOREIGN KEY (team) REFERENCES Clubs(teamnr)
+);
 
 CREATE TABLE Cheftrainer (
     trainernr INT AUTO_INCREMENT PRIMARY KEY,
+    team INT NOT NULL,
     vorname VARCHAR(20),
     nachname VARCHAR(20),
-    FOREIGN KEY (team) REFERENCES Club(teamnr)
-    );
-
-CREATE TABLE Liga (
-    liganr INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    land VARCHAR(20)
-    );
-
-
+    FOREIGN KEY (team) REFERENCES Clubs(teamnr)
+);
