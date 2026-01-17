ce39887762c4fb5687bc48254405b05d0d39b6eb
nepomukharnoncourt-afk <nepomuk.harnoncourt@mng.ch>
2026-01-17 07:14:08 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -509,7 +509,7 @@ def adminarea():
         # ============================
         # NEW: insert a row
         # ============================
-               elif action == "insert":
+        elif action == "insert":
             table = request.form.get("table")
             q = (request.form.get("q") or "").strip()
 
@@ -586,3 +586,4 @@ def adminarea():
 
 if __name__ == "__main__":
     app.run()
+
