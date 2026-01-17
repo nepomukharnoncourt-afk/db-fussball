65b9d73c3ccc56ae710b3900cad3722fd3bc78c8
nepomukharnoncourt-afk <nepomuk.harnoncourt@mng.ch>
2026-01-17 07:03:19 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -54,16 +54,16 @@ CREATE TABLE Cheftrainer (
 
 
 
-INSERT INTO Liga (name, Land) Values
-('Bundesliga', 'Deutschland')
-('Laliga', 'Spanien')
+INSERT INTO Liga (name, land) Values
+('Bundesliga', 'Deutschland'),
+('Laliga', 'Spanien');
 
 INSERT INTO Clubs (liga, tore, gegentore, name, platzierung) Values
-(1, 66, 9, 'test', 2)
-(2, 7, 6, 'test2', 3)
+(1, 66, 9, 'test', 2),
+(2, 7, 6, 'test2', 3);
 
 INSERT INTO Spieler (team, vorname, nachname, tore, vorlagen, marktwert, position) Values
-(2, 'nnn', 'hhh', 2, 0, 4000000, 'verteidiger')
+(2, 'nnn', 'hhh', 2, 0, 4000000, 'verteidiger');
 
 INSERT INTO Cheftrainer (team, vorname, nachname) Values
-(2, 'nnnn', 'hhhh')
+(2, 'nnnn', 'hhhh');
